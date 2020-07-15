9d82cc758dca1c9101e245b6bd505f195447e29a
Amit Agrawal <amit5111998@gmail.com>
2020-07-16 00:04:25 +0530

    changed database to postdress and added home page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    imag = models.ImageField(upload_to = 'images/')
